5d300a42d56810adfc402474f401a2f137609d6b
Aadit Shah <aadit2805@gmail.com>
2025-04-10 12:07:33 -0500

    Initial Commit

---
+++ geekd/geekdApp.swift
@@ -0,0 +1,17 @@
+//
+//  geekdApp.swift
+//  geekd
+//
+//  Created by Aadit Shah on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct geekdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ geekdTests/geekdTests.swift
@@ -0,0 +1,17 @@
+//
+//  geekdTests.swift
+//  geekdTests
+//
+//  Created by Aadit Shah on 4/10/25.
+//
+
+import Testing
+@testable import geekd
+
+struct geekdTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
